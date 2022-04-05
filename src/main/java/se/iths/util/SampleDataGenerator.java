6bb70879770f2ca5e143eadcd70ecb8bf0a1eb91
milan <milanarif@gmail.com>
2022-04-05 12:48:03 +0200

    minor fixes

--- src/main/java/se/iths/util/SampleDataGenerator.java
+++ src/main/java/se/iths/util/SampleDataGenerator.java
@@ -1,9 +1,8 @@
-
-  
 package se.iths.util;
 
-import se.iths.entity.Item;
-import se.iths.entity.User;
+import se.iths.entity.Student;
+import se.iths.entity.Subject;
+import se.iths.entity.Teacher;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -21,8 +20,8 @@ public class SampleDataGenerator {
     @PostConstruct
     public void generateData() {
 
-        Student student1 = new Student("Kalle", "Anka" "kalle@ankeborg.se");
-        Student student2 = new Student("Mona", "Sahlin", "mona@toblerone.se");
+        Student student1 = new Student("Kalle", "Anka", "kalle@ankeborg.se", "080808");
+        Student student2 = new Student("Mona", "Sahlin", "mona@toblerone.se", "0707070");
 
         Teacher teacher1 = new Teacher("Bosse", "Karlsson");
         Teacher teacher2 = new Teacher("Berit", "Bagare");
