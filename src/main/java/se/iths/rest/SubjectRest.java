d4748a6f2f00fa847fcffef85986e77654c95b9b
PL <philip.lundin@iths.se>
2022-04-05 14:21:48 +0200

    subject error handling

--- src/main/java/se/iths/rest/SubjectRest.java
+++ src/main/java/se/iths/rest/SubjectRest.java
@@ -4,7 +4,9 @@ import se.iths.HttpError;
 import se.iths.entity.Student;
 import se.iths.entity.Subject;
 import se.iths.entity.Teacher;
+import se.iths.service.StudentService;
 import se.iths.service.SubjectService;
+import se.iths.service.TeacherService;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -12,7 +14,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Path("subjects")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -22,18 +23,24 @@ public class SubjectRest {
     @Inject
     SubjectService subjectService;
 
+    @Inject
+    StudentService studentService;
+
+    @Inject
+    TeacherService teacherService;
+
     @POST
     public Response createSubject(Subject subject) {
         if (subject.getName() == null) {
             throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity("include name").type(MediaType.TEXT_PLAIN_TYPE).build());
         } else {
-          subjectService.createSubject(subject);
-          return Response.status(201).entity(subject).build();
+            subjectService.createSubject(subject);
+            return Response.status(201).entity(subject).build();
         }
     }
 
     @GET
-    public Response getAllSubjects(){
+    public Response getAllSubjects() {
         List<Subject> foundSubjects = subjectService.getAllSubjects();
 
         if (foundSubjects.size() > 0) {
@@ -45,20 +52,29 @@ public class SubjectRest {
 
     @Path("{id}")
     @GET
-    public Response getSubjectById(@PathParam("id") Long id){
+    public Response getSubjectById(@PathParam("id") Long id) {
         Subject foundSubject = subjectService.getSubjectById(id);
         if (foundSubject == null) {
-            //throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity("Subject by that ID: " + id + " was not found.").type(MediaType.APPLICATION_JSON).build());
             return Response.status(404).entity(new HttpError(404, "Subject by that ID: " + id + " was not found.")).type(MediaType.APPLICATION_JSON
             ).build();
         } else {
             List<Student> enrolledStudents = subjectService.getEnrolledStudents(id);
             Teacher assignedTeacher = subjectService.getTeacher(id);
+            String teacher;
             List<String> students = new ArrayList<>();
-            enrolledStudents.stream().forEach(s -> students.add(s.getFirstName() + " " + s.getLastName()));
 
+            if (enrolledStudents.size() == 0) {
+                students.add("No students added to subject");
+            } else {
+                enrolledStudents.stream().forEach(s -> students.add(s.getFirstName() + " " + s.getLastName()));
+            }
 
-            return Response.ok("Subject: " + foundSubject.getName() + "\nTeacher: " + assignedTeacher.getFirstName() + " " +  assignedTeacher.getLastName() + "\nStudents: " + students).build();
+            if (assignedTeacher == null) {
+                teacher = "No teacher added to subject";
+            } else {
+                teacher = assignedTeacher.getFirstName() + " " + assignedTeacher.getLastName();
+            }
+            return Response.ok("Subject: " + foundSubject.getName() + "\nTeacher: " + teacher + "\nStudents: " + students).build();
         }
     }
 
@@ -67,7 +83,8 @@ public class SubjectRest {
     public Response deleteSubject(@PathParam("id") Long id) {
         Subject subject = subjectService.deleteSubject(id);
         if (subject == null) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity("Subject by that ID: " + id + " was not found.").type(MediaType.TEXT_PLAIN_TYPE).build());
+            return Response.status(404).entity(new HttpError(404, "Subject by that ID: " + id + " was not found.")).type(MediaType.APPLICATION_JSON
+            ).build();
         } else {
             return Response.ok("Subject " + subject.getName() + " was successfully removed.").type(MediaType.TEXT_PLAIN_TYPE).build();
         }
@@ -76,16 +93,37 @@ public class SubjectRest {
     @Path("/enroll/{id}")
     @POST
     public Response enrollStudent(@PathParam("id") Long subjectId, Student student) {
-        subjectService.enrollStudent(subjectId, student.getId());
 
-        return Response.status(Response.Status.OK).entity("Student enrolled to course " + subjectId).build();
+        Subject foundSubject = subjectService.getSubjectById(subjectId);
+        Student foundStudent = studentService.getStudentById(student.getId());
+
+        if (foundSubject == null) {
+            return Response.status(404).entity(new HttpError(404, "Subject by that ID: " + subjectId + " was not found.")).type(MediaType.APPLICATION_JSON
+            ).build();
+        } else if (foundStudent == null) {
+            return Response.status(404).entity(new HttpError(404, "Student by that ID: " + student.getId() + " was not found.")).type(MediaType.APPLICATION_JSON
+            ).build();
+        } else {
+            subjectService.enrollStudent(subjectId, student.getId());
+            return Response.status(Response.Status.OK).entity("Student enrolled to course " + subjectId).build();
+        }
     }
 
     @Path("/assign/{id}")
     @POST
     public Response assignTeacher(@PathParam("id") Long subjectId, Teacher teacher) {
-        subjectService.assignTeacher(subjectId, teacher.getId());
+        Subject foundSubject = subjectService.getSubjectById(subjectId);
+        Teacher foundTeacher = teacherService.getTeacherById(teacher.getId());
 
-        return Response.status(Response.Status.OK).entity("Teacher enrolled to course " + subjectId).build();
+        if (foundSubject == null) {
+            return Response.status(404).entity(new HttpError(404, "Subject by that ID: " + subjectId + " was not found.")).type(MediaType.APPLICATION_JSON
+            ).build();
+        } else if (foundTeacher == null) {
+            return Response.status(404).entity(new HttpError(404, "Teacher by that ID: " + teacher.getId() + " was not found.")).type(MediaType.APPLICATION_JSON
+            ).build();
+        } else {
+            subjectService.assignTeacher(subjectId, teacher.getId());
+            return Response.status(Response.Status.OK).entity("Teacher enrolled to course " + subjectId).build();
+        }
     }
 }
